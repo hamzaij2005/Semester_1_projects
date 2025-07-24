de9086671bee79d5ae075e916ae92e642a87b6f5
“codealpha_tasks <ijazhamza2005@gmail.com>
2025-07-24 13:42:06 +0500

    Leap_Year_checking

---
+++ CPP_MINI_PROJECTS/Leap_year_checking.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int year;
+    cout << "Enter year: ";
+    cin >> year;
+    if (year % 4 == 0){
+        cout<<"=========================================="<<endl;
+        cout << year <<" is a leap year";
+        cout << "==========================================" << endl;
+    }
+    else{
+        cout << "==========================================" << endl;
+        cout << year<<"is not a leap year";
+        cout << "==========================================" << endl;
+    }
+    return 0;
+}
